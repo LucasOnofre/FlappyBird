748f04c591394bff6b82469602b17a2516343719
Lucas <lucas-onofre@hotmail.com>
2018-02-26 16:54:07 -0300

    Quinto

--- core/src/com/flappybird/FlappyBird.java
+++ core/src/com/flappybird/FlappyBird.java
@@ -3,7 +3,6 @@ package com.flappybird;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -16,186 +15,137 @@ import java.util.Random;
 
 public class FlappyBird extends ApplicationAdapter {
 
-    //CLASSE USADA PARA ANIMAÇÕES
-
-    private SpriteBatch batch;
-
-    private Boolean marcou = false;
-
     private Texture     fundo;
-    private Texture     canoTop;
+    private Texture     canoTopo;
     private Texture     canoBaixo;
     private Texture[]   passaros;
 
-    private int pontuacao = 0;
-    private int estadoJogo = 0;
-    private int alturaDispositivo;
-    private int larguraDispositivo;
+    private Random      numeroRandomico;
 
-    private float deltaTime;
-    private float espacoCanos;
-    private float variacao = 0;
-    private float velocidadeQueda = 0;
-    private float posicaoInicialVertical;
-    private float alturaEntreCanosRandomica;
-    private float posicaoMovimentoCanoHorizontal;
+    private Circle      passaroCirculo;
+    private Rectangle   retanguloCanoTopo;
+    private Rectangle   retanguloCanoBaixo;
 
-    private Random numeroRandomico;
+    private BitmapFont      fonte;
+    private SpriteBatch     batch;
+    private ShapeRenderer   shape;
 
-    private BitmapFont fonte;
 
-    private Circle  passaroCircle;
+    //Atributos de configuracao
+    private int     pontuacao = 0;
+    private int     estadoJogo = 0;
+    private int     alturaDispositivo;
+    private int     larguraDispositivo;
 
-    private Rectangle   recCanoTopo;
-    private Rectangle   recCanoBottom;
+    private float   deltaTime;
+    private float   variacao = 0;
+    private float   espacoEntreCanos;
+    private float   velocidadeQueda = 0;
+    private float   posicaoInicialVertical;
+    private float   alturaEntreCanosRandomica;
+    private float   posicaoMovimentoCanoHorizontal;
 
-    private ShapeRenderer shapeRenderer;
+    private boolean marcouPonto = false;
 
 
     @Override
-    public void create() {
-
-
-        //VARIAVEIS QUE PEGAM A LARGURA E ALTURA DA TELA
-
-        larguraDispositivo  = Gdx.graphics.getWidth();
-        alturaDispositivo   = Gdx.graphics.getHeight();
-
-        //VARIAVEL QUE PEGA O MEIO DA TELA, ONDE IRÁ INICIAR O PASSARO
-
-        posicaoInicialVertical = alturaDispositivo / 2;
-
-        //INICIALIZAÇÃO DA FONTE E CONFIGURAÇÃO DA FONTE
-
-        fonte =  new BitmapFont();
-        fonte.setColor(Color.WHITE);
-        fonte.getData().setScale(7);
-
-        //FORMAS PARA COLISÃO
-
-        passaroCircle      = new Circle();
-        recCanoTopo        = new Rectangle();
-        recCanoBottom      = new Rectangle();
-
-
-
-
-
-        //INICIALIZAÇÃO DAS CLASSES DA BIBLIOTECA
+    public void create () {
 
         batch = new SpriteBatch();
 
-        //INICIALIZAÇÃO DOS CANOS
+        //INICIALIZANDO AS FORMAS
+        numeroRandomico     = new Random();
+        passaroCirculo      = new Circle();
+        retanguloCanoTopo   = new Rectangle();
+        retanguloCanoBaixo  = new Rectangle();
 
-        fundo       = new Texture("fundo.png");
-        canoTop     = new Texture("cano_topo_maior.png");
-        canoBaixo   = new Texture("cano_baixo_maior.png");
+        shape = new ShapeRenderer();
 
-
-        //NUMERO RANDOMICO
-        numeroRandomico = new Random();
-
-        //INICIALIZAÇÃO DA VARIAVEL PASSARO  QUE USA 3 IMAGENS PARA FAZER A IMPRESSÃO DE BATER DE ASAS
+        //CONFIGURANDO E INICIALIZANDO A FONTE
+        fonte = new BitmapFont();
+        fonte.setColor(Color.WHITE);
+        fonte.getData().setScale(7);
 
         passaros    = new Texture[3];
         passaros[0] = new Texture("passaro1.png");
         passaros[1] = new Texture("passaro2.png");
         passaros[2] = new Texture("passaro3.png");
 
-        espacoCanos = 250;
+        fundo       = new Texture("fundo.png");
+        canoBaixo   = new Texture("cano_baixo_maior.png");
+        canoTopo    = new Texture("cano_topo_maior.png");
 
-        posicaoMovimentoCanoHorizontal = larguraDispositivo - 100;
+        larguraDispositivo      = Gdx.graphics.getWidth();
+        alturaDispositivo       = Gdx.graphics.getHeight();
+        posicaoInicialVertical  = alturaDispositivo / 2;
+
+        posicaoMovimentoCanoHorizontal = larguraDispositivo;
+        espacoEntreCanos = 300;
+
     }
 
     @Override
-    public void render() {
-
-        //RECUPERA O DELTA TIME
+    public void render () {
 
         deltaTime = Gdx.graphics.getDeltaTime();
-        variacao += deltaTime * 9;
+        variacao += deltaTime * 10;
 
-        //CONDIÇÃO PARA O BATER DE ASAS DO PASSARO SER INFINITO
+        if (variacao > 2) variacao = 0;
 
-        if (variacao > 2) {
+        if( estadoJogo == 0 ){//Não iniciado
 
-            variacao = 0;
-        }
-        //VERIFICA SE O JOGO INICIOU
-
-        if (estadoJogo == 0) {
-            if (Gdx.input.justTouched()) {
+            if( Gdx.input.justTouched() ){
                 estadoJogo = 1;
             }
-        } else {
 
-            posicaoMovimentoCanoHorizontal -= deltaTime * 400;
+        }else {
 
+            posicaoMovimentoCanoHorizontal -= deltaTime * 450;
             velocidadeQueda++;
 
-            variacao += deltaTime * 9;
+            if (Gdx.input.justTouched()) {
+                velocidadeQueda = -15;
+            }
 
-            //FAZ O CANO REPETIR AO PASSAR NA TELA
+            if (posicaoInicialVertical > 0 || velocidadeQueda < 0)
+                posicaoInicialVertical = posicaoInicialVertical - velocidadeQueda;
 
-            if (posicaoMovimentoCanoHorizontal < -canoTop.getWidth()) {
+            //Verifica se o cano saiu inteiramente da tela
+            if (posicaoMovimentoCanoHorizontal < -canoTopo.getWidth()) {
                 posicaoMovimentoCanoHorizontal  = larguraDispositivo;
-                alturaEntreCanosRandomica       = numeroRandomico.nextInt(550) - 200;
-                marcou = false;
-
+                alturaEntreCanosRandomica       = numeroRandomico.nextInt(400) - 200;
+                marcouPonto = false;
             }
 
-            //CONDIÇÃO QUE FAZ O PASSARO CAIR, TIRANDO DA POSICAO INICIAL
-
-            if (posicaoInicialVertical > 0 || velocidadeQueda < 0) {
-                posicaoInicialVertical -= velocidadeQueda;
-            }
-
-            //CONDIÇÃO PARA PONTUAÇÃO
-
-            if (posicaoMovimentoCanoHorizontal < 120){
-                if (!marcou){
+            //Verifica pontuação
+            if(posicaoMovimentoCanoHorizontal < 120 ){
+                if( !marcouPonto ){
                     pontuacao++;
-                    marcou = true;
+                    marcouPonto = true;
                 }
             }
-
-            //CONDIÇÃO QUE PEGA QUANDO A TELA É TOCADA E DECRSCENTA  DA VELOCIDADE DE QUEDA
-            //PARA QUE ELA DIMINUA E DE A IMPRESSÃO DE QUE O PASSARO SUBIU DE NOVO
-
-            if (Gdx.input.justTouched()) {
-                velocidadeQueda = -17;
-            }
-
-            //COMEÇANDO A BIBLIOTECA E A ANIMAÇÃO
         }
 
         batch.begin();
 
-        //DESENHANDO O FUNDO DO APP, PEGANDO ALTURA E LARGURA DA TELA E ETC
-
         batch.draw(fundo, 0, 0, larguraDispositivo, alturaDispositivo);
 
-        //DESENHANDO O CANOS
-
-        batch.draw(canoTop, posicaoMovimentoCanoHorizontal, alturaDispositivo / 2 + espacoCanos / 2 + alturaEntreCanosRandomica);
-        batch.draw(canoBaixo, posicaoMovimentoCanoHorizontal, alturaDispositivo / 2 - canoBaixo.getHeight() - espacoCanos / 2 + alturaEntreCanosRandomica);
-
-        //DESENHANDO O PASSARO, PEGANDO A POSICAO DO MESMO E USANDO A VARIAÇÃO PARA TER O BATER DE ASAS
-
+        //canos e passaros
+        batch.draw(canoTopo, posicaoMovimentoCanoHorizontal, alturaDispositivo / 2 + espacoEntreCanos / 2 + alturaEntreCanosRandomica);
+        batch.draw(canoBaixo, posicaoMovimentoCanoHorizontal, alturaDispositivo / 2 - canoBaixo.getHeight() - espacoEntreCanos / 2 + alturaEntreCanosRandomica);
         batch.draw(passaros[(int) variacao], 120, posicaoInicialVertical);
 
-        //DESENHANDO A FONTE
-
-        fonte.draw(batch,String.valueOf(pontuacao),larguraDispositivo/2,alturaDispositivo -50);
-
-        //Desenhando formas
-
-        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-        shapeRenderer.end();
-
-        //FINALIZANDO A ANIMAÇÃO
+        //fonte
+        fonte.draw(batch, String.valueOf(pontuacao), larguraDispositivo / 2, alturaDispositivo - 50);
 
         batch.end();
+
+        passaroCirculo.set(120 + passaros[0].getWidth() / 2, posicaoInicialVertical + passaros[0].getHeight() / 2, passaros[0].getWidth() / 2);
+        retanguloCanoBaixo = new Rectangle();
+        //Desenhar formas
+        shape.begin( ShapeRenderer.ShapeType.Filled);
+        shape.circle(passaroCirculo.x, passaroCirculo.y, passaroCirculo.radius);
+        shape.setColor(Color.RED);
+        shape.end();
     }
 }
-
